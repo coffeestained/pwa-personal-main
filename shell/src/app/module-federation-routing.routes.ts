9aa4ff968684a89feef5a7b95493b8125e66e4c7
Matthew Grady <matthewbgrady90@gmail.com>
2025-03-27 20:01:50 -0400

    chore/react-mfe-support

--- shell/src/app/module-federation-routing.routes.ts
+++ shell/src/app/module-federation-routing.routes.ts
@@ -10,55 +10,54 @@ export declare type SpaRoute = Route & { isNavigation?: string, displayName?: st
 export declare type SpaRoutes = SpaRoute[];
 
 export function buildRoutes(): SpaRoutes {
-
     const lazyRoutes = Object.entries(getManifest<CustomManifest>())
-    .filter(([_, value]: any) => {
-        return value.viaRoute === true
-    }).map(([key, value]: any) => {
-      const route = {
-        path: value.routePath,
-        isNavigation: value.isNavigation,
-        displayName: value.displayName,
-      } as any;
+      .filter(([_, value]: any) => {
+          return value.viaRoute === true
+      }).map(([key, value]: any) => {
+        const route = {
+          path: value.routePath,
+          isNavigation: value.isNavigation,
+          displayName: value.displayName,
+        } as any;
 
-      if (value.ngModuleName) {
-        const remoteModuleOptions : any = {
-          type: 'manifest',
-          remoteName: key,
-          exposedModule: value.exposedModule,
-        };
-        let loader = () => loadRemoteModule(
-          remoteModuleOptions
-        ).then((m: any) => { 
-          return m[value.ngModuleName!] 
-        });
-        route['loadChildren'] = loader;
-      } else if (value.elementName) {
-        route.component = WebComponentWrapper;
-        route.data = {
-          remoteEntry: value.remoteEntry,
-          remoteName: key,
-          exposedModule: value.exposedModule,
-          elementName: value.elementName,
-        } as WebComponentWrapperOptions;
-        Array.isArray(value.children) ? 
-          route.children = value.children.map((child: any) => {
-            return {
-              path: child.routePath,
-              isNavigation: child.isNavigation,
-              displayName: child.displayName,
-              component: WebComponentWrapper,
-              data: {
-                remoteEntry: child.remoteEntry,
-                remoteName: key,
-                exposedModule: child.exposedModule,
-                elementName: child.elementName,
-              } as WebComponentWrapperOptions
-            }
-          }) : null;
-      }
-      return route;
-    });
+        if (value.ngModuleName) {
+          const remoteModuleOptions : any = {
+            type: 'manifest',
+            remoteName: key,
+            exposedModule: value.exposedModule,
+          };
+          let loader = () => loadRemoteModule(
+            remoteModuleOptions
+          ).then((m: any) => { 
+            return m[value.ngModuleName!] 
+          });
+          route['loadChildren'] = loader;
+        } else if (value.elementName) {
+          route.component = WebComponentWrapper;
+          route.data = {
+            remoteEntry: value.remoteEntry,
+            remoteName: key,
+            exposedModule: value.exposedModule,
+            elementName: value.elementName,
+          } as WebComponentWrapperOptions;
+          Array.isArray(value.children) ? 
+            route.children = value.children.map((child: any) => {
+              return {
+                path: child.routePath,
+                isNavigation: child.isNavigation,
+                displayName: child.displayName,
+                component: WebComponentWrapper,
+                data: {
+                  remoteEntry: child.remoteEntry,
+                  remoteName: key,
+                  exposedModule: child.exposedModule,
+                  elementName: child.elementName,
+                } as WebComponentWrapperOptions
+              }
+            }) : null;
+        }
+        return route;
+      });
 
     const notFound = [
       {
@@ -66,7 +65,7 @@ export function buildRoutes(): SpaRoutes {
         component: PageNotFoundComponent,
       }
     ];
-
+    console.log(lazyRoutes);
     const combinedRoutes =  [...routes, ...lazyRoutes, ...notFound];
     return combinedRoutes as SpaRoutes;
 }
